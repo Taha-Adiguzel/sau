a07a747a410749eca410adcf819a22e548d19807
Taha-Adiguzel <taha.adiguzel@hotmail.com>
2024-03-04 14:40:22 +0300

    İsimlerin getirilmesi eklendi

--- src/Lexical.py
+++ src/Lexical.py
@@ -1,9 +1,10 @@
 import nltk
+import spacy
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 
 nltk.download('averaged_perceptron_tagger')
-
+nlp = spacy.load("en_core_web_sm")
 class Lexical:
     def __init__(self,text):
         self.text = text
@@ -14,3 +15,8 @@ class Lexical:
 
         verbs = [word for word,tag in tagged_words if tag.startswith('VB')]
         return verbs
+    
+    def get_people_names(self):
+        doc = nlp(self.text)
+        names = [entity.text for entity in doc.ents if entity.label_ == "PERSON"]
+        return names
--- src/main.py
+++ src/main.py
@@ -3,3 +3,4 @@ from Lexical import Lexical
 txt = input('Enter Text:')
 lexical = Lexical(txt)
 print("Verbs in text: ",lexical.get_verbs())
+print("Names: ",lexical.get_people_names())
